f9ba8a94b4ab45f7acadae396672ffc9fb5b4ed2
Prasanth-dev11 <156569201+Prasanth-dev11@users.noreply.github.com>
2024-01-14 03:58:00 -0600

    Add files via upload

---
+++ EVENODDD.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int given_number;
+    scanf("%d",&given_number);
+    if(given_number%2==0)
+    {
+        printf("The number is an even number");
+    }
+    else
+    {
+        printf("The number is an odd number");
+    }
+    return 0;
+
+}
